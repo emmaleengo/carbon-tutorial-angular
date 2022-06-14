e167b79e3bd163e07528b0563cab29d71a0fea7a
Emily Ngo <73451814+emmaleengo@users.noreply.github.com>
2022-06-13 20:17:07 -0400

    Additions
    Added more information from the angular tutorial

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
